6d3083c63c8d901bf81c8cbd64701ca46a74a7d0
kduane <karl.duane@gmail.com>
2018-06-17 20:26:21 -0400

    3/6 regex tests passing

--- js/src/core/regex.js
+++ js/src/core/regex.js
@@ -1,24 +1,30 @@
 containsNumber = (string) => {
-  let regex = /regex/
-  let found = string.match(regex)
+  // var regex = /\d+/g;
+  // var found = string.match(regex);
+  // if (isNaN(found) == true) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  return /\d/.test(string);
 };
 
 containsRepeatingLetter = (string) => {
-
+  return (/([a-z])\1/i).test(string);
 };
 
 endsWithVowel = (string) => {
-
+  return (/[aeiou]$/i).test(string);
 };
 
 captureThreeNumbers = (string) => {
-
+  return (/\d{3}/).test(string);
 };
 
 matchesPattern = (string) => {
-
+  // return ().test(string);
 };
 
 isUSD = (string) => {
-
+  // return ().test(string);
 };
