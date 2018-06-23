9855ff8a2ab15c90e2e42719a9aacdf52f4c7920
kduane <karl.duane@gmail.com>
2018-06-23 14:58:10 -0400

    string tests passing, 24/32

--- js/src/core/strings.js
+++ js/src/core/strings.js
@@ -1,7 +1,24 @@
 reduceString = (str, amount) => {
-
+  var count = 0;
+  var result = '';
+  for (var i = 0; i< str.length; i++) {
+    if (str[i] === str[i+1]) {
+      count ++;
+      if (count < amount) {
+        result += str[i];
+      }
+    } else {
+      count = 0;
+      result += str[i];
+    }
+  }
+  return result;
 };
 
 reverseString = (str) => {
-
+  var result = '';
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
 };
